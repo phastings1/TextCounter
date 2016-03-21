f673840e9217c89de3baf203f9c3b7b935666323
phastings1 <patrickhasting@gmail.com>
2016-03-21 11:32:00 -0400

    Added files via upload
    CRreport data found log writer in progress

---
+++ CRreportscanner.py
@@ -0,0 +1,19 @@
+#3-18-2016 take two, master report scanner hand coded PH
+#import time so we can get the time stamp on the write file
+import time
+#concatenate variable with literal string here for the print function
+'''result1= 0
+matra = 'The total number of allocations is:'
+rslt1prnt = (matra, result1)
+print (rslt1prnt)
+
+#time stamp in string
+timestr = time.strftime("%Y%m%d-%H%M%S")
+filename= 
+
+
+writeMe = 'CRreportscanner'
+
+saveFile = open('exampleWrite.txt','w')
+saveFile.write(writeMe)'''
+#################################################
